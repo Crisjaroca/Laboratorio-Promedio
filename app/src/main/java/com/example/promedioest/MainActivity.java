84a2221ecb3f4c3e4c22c0588d3bc9901930c261
Crisjaroca <crijavrodriguez@uniboyaca.edu.co>
2022-10-09 21:14:02 -0500

    Creación segunda actividad

--- app/src/main/java/com/example/promedioest/MainActivity.java
+++ app/src/main/java/com/example/promedioest/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.promedioest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button continuarBtn;
+    private EditText nombreEst;
+    private EditText codigoEst;
+    private Spinner materiasEst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreEst = findViewById(R.id.nombreEst);
+        codigoEst = findViewById(R.id.codigoEst);
+        materiasEst = findViewById(R.id.materiasEst);
+
+        this.continuarBtn = findViewById(R.id.continuarBtn);
+        this.continuarBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, DatosActivity.class);
+            startActivity(intent);
+        });
     }
 }
