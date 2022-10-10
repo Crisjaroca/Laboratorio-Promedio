c2374252a47ad723f701e4a9bc68205c79d2acd6
Crisjaroca <crijavrodriguez@uniboyaca.edu.co>
2022-10-10 01:00:38 -0500

    Diseño Interfaces v0.2

--- app/src/main/java/com/example/promedioest/InformeActivity.java
+++ app/src/main/java/com/example/promedioest/InformeActivity.java
@@ -2,26 +2,37 @@ package com.example.promedioest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class InformeActivity extends AppCompatActivity {
     private Button volverBtnInfo;
     private Button calcularBtn;
 
-    private EditText nombreInfo;
-    private EditText codigoInfo;
-    private EditText materiaInfo;
+    private TextView nombreInfo;
+    private TextView codigoInfo;
+    private TextView materiaInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informe);
 
-        this.volverBtnInfo = findViewById(R.id.volverBtnInfo);
+        nombreInfo = findViewById(R.id.nombreInfo);
+        codigoInfo = findViewById(R.id.codigoInfo);
+        materiaInfo = findViewById(R.id.materiaInfo);
+
+        String nombre = getIntent().getStringExtra("NOMBRE_KEY");
+        String codigo = getIntent().getStringExtra("CODIGO_KEY");
+        String materia = getIntent().getStringExtra("MATERIA_KEY");
+        nombreInfo.setText(nombre);
+        codigoInfo.setText(codigo);
+        materiaInfo.setText(materia);
+
+        this.volverBtnInfo = findViewById(R.id.continuarBtn);
         this.volverBtnInfo.setOnClickListener(view -> {
             Intent intent = new Intent(this, MainActivity.class);
             finish();
